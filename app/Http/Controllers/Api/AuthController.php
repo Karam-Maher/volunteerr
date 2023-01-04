bec4ed5e8cd70c9f7c1fcd975ff95eeec67a99c8
Karam-Maher <karammaheris05@gmail.com>
2023-01-04 16:55:20 +0200

    api v3

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -2,12 +2,12 @@
 
 namespace App\Http\Controllers\Api;
 
-use Carbon\Carbon;
+
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Auth\AuthenticationException;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -36,21 +36,39 @@ class AuthController extends Controller
             'user' => $user
         ]);
     }
+
     public function login(Request $request)
     {
-        $this->validate($request,[
-            'email' =>'required|email',
-            'password' => 'required'
-        ]);
-        if(!auth()->attempt($request->only('email','password'))){
-            throw new AuthenticationException();
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){
+            $user = Auth::user();
+            $success['token'] =  $user->createToken('MyApp')->plainTextToken;
+            $success['name'] =  $user->name;
+            $success['email'] =  $user->email;
+            $success['active'] =  $user->active;
+            return  response()->json([
+                 'message' => 'User login successfully.',
+                 'user' => $success
+            ]);
         }
-        $user =  $request->user();
-        $token = auth()->user()->createToken('Per')->plainTextToken;
-        return [
-            'token' =>  $token,
-            'user' => $user
-        ];
+        else{
+            return  response()->json([
+                'message' => 'Unauthorised',
+            ]);
+
+        }
+        // $user = User::where('email', $request->email )->first();
+        // if(!$user||!Hash::check($request->password, $user->password)) {
+        //     return  response()->json([
+        //         'message' => ['These credentials do not match our records']
+        //     ], 404);
+        // }
+
+        // $token = $user->createToken('my-app-token')->plainTextToken;
+        // $response =[
+        //     'user' => $user,
+        //     'token' => $token
+        // ];
+        // return response($response,201);
     }
 
     public function profile()
